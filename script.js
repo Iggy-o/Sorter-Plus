bee04799710718369a6805625c74249d0c25c076
Ighoise O <iodigie03@gmail.com>
2020-06-04 03:53:52 +0000

    Maintenance Update

--- script.js
+++ script.js
@@ -285,30 +285,36 @@ async function radixSort() {
     for (let i = 0; i <= arr.length-1; i++) {
       digit = await simplify(arr[i].height, value)
       countArr[digit] = countArr[digit] + 1
-      console.log(arr[i].height)
+      //console.log(arr[i].height)
     }
-    console.log(countArr)
+    //console.log(countArr)
     countArr[0]--
     for (let i = 1; i < 10; i++) {
       countArr[i]+=countArr[i-1]
     }
-    console.log(countArr)
+    //console.log(countArr)
     for (let i = arr.length - 1; i >= 0; i--) {
       digit = await simplify(arr[i].height, value)
       auxArr[i] = countArr[digit]      
       countArr[digit] = countArr[digit] - 1
     }
-    console.log('aux', auxArr)
+    //console.log('aux', auxArr)
 
-    /*for (let g = 0; g<2; g++) {
-      let i = 0; 
-      while (i < arr.length) {
-        if (auxArr[i] != i) {
-          await swap(auxArr[i], i)
-        }
+    let i = 0;
+    let last = arr.length - 1 
+    while (i < arr.length + 1) {
+      if (auxArr[last] != last) {
+        await swap(auxArr[last], last)
+        temp = auxArr[last]
+        auxArr[last] = auxArr[auxArr[last]]
+        auxArr[auxArr[last]] = temp
         i++
       }
-    }*/
+      else {
+        console.log("down")
+        last--
+      }
+    }
   }
   function simplify(number, value) {
     return Math.floor(number%(value*10)/value)
